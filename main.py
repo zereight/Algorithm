64546db16886cd03af715d8cad988bcee36033cb
fullstackmachin <fullstackmachinelearning@gmail.com>
2020-09-22 13:55:04 +0000

    1753 최단경로 (간단한 다익스트라 시간초과)

--- main.py
+++ main.py
@@ -1,34 +1,78 @@
 '''
 문제
-어떤 나라에는 1번부터 N번까지의 도시와 M개의 단방향 도로가 존재한다. 모든 도로의 거리는 1이다.
-
-이 때 특정한 도시 X로부터 출발하여 도달할 수 있는 모든 도시 중에서, 최단 거리가 정확히 K인 모든 도시들의 번호를 출력하는 프로그램을 작성하시오. 또한 출발 도시 X에서 출발 도시 X로 가는 최단 거리는 항상 0이라고 가정한다.
-
-예를 들어 N=4, K=2, X=1일 때 다음과 같이 그래프가 구성되어 있다고 가정하자.
-
-
-
-이 때 1번 도시에서 출발하여 도달할 수 있는 도시 중에서, 최단 거리가 2인 도시는 4번 도시 뿐이다.  2번과 3번 도시의 경우, 최단 거리가 1이기 때문에 출력하지 않는다.
+방향그래프가 주어지면 주어진 시작점에서 다른 모든 정점으로의 최단 경로를 구하는 프로그램을 작성하시오. 단, 모든 간선의 가중치는 10 이하의 자연수이다.
 
 입력
-첫째 줄에 도시의 개수 N, 도로의 개수 M, 거리 정보 K, 출발 도시의 번호 X가 주어진다. (2 ≤ N ≤ 300,000, 1 ≤ M ≤ 1,000,000, 1 ≤ K ≤ 300,000, 1 ≤ X ≤ N) 둘째 줄부터 M개의 줄에 걸쳐서 두 개의 자연수 A, B가 공백을 기준으로 구분되어 주어진다. 이는 A번 도시에서 B번 도시로 이동하는 단방향 도로가 존재한다는 의미다. (1 ≤ A, B ≤ N) 단, A와 B는 서로 다른 자연수이다.
+첫째 줄에 정점의 개수 V와 간선의 개수 E가 주어진다. (1≤V≤20,000, 1≤E≤300,000) 모든 정점에는 1부터 V까지 번호가 매겨져 있다고 가정한다. 둘째 줄에는 시작 정점의 번호 K(1≤K≤V)가 주어진다. 셋째 줄부터 E개의 줄에 걸쳐 각 간선을 나타내는 세 개의 정수 (u, v, w)가 순서대로 주어진다. 이는 u에서 v로 가는 가중치 w인 간선이 존재한다는 뜻이다. u와 v는 서로 다르며 w는 10 이하의 자연수이다. 서로 다른 두 정점 사이에 여러 개의 간선이 존재할 수도 있음에 유의한다.
 
 출력
-X로부터 출발하여 도달할 수 있는 도시 중에서, 최단 거리가 K인 모든 도시의 번호를 한 줄에 하나씩 오름차순으로 출력한다.
+첫째 줄부터 V개의 줄에 걸쳐, i번째 줄에 i번 정점으로의 최단 경로의 경로값을 출력한다. 시작점 자신은 0으로 출력하고, 경로가 존재하지 않는 경우에는 INF를 출력하면 된다.
 
-이 때 도달할 수 있는 도시 중에서, 최단 거리가 K인 도시가 하나도 존재하지 않으면 -1을 출력한다.
-
-4 4 2 1
-1 2
-1 3
-2 3
-2 4
+5 6
+1
+5 1 1
+1 2 2
+1 3 3
+2 3 4
+2 4 5
+3 4 6
 '''
 import sys
 input = sys.stdin.readline
-n,m,k,x = map(int, input().split(" "))
-line = []
-for _ in range(m):
-  start, end = map(int, input().split(" "))
-  line.append()
 
+V,E = map(int, input().split(" "))
+k = int(input())
+lines = dict()
+for i in range(0,V+1):
+  lines[i] = []
+
+INF = int(1e9)
+for _ in range( E ):
+  u, v, w = map(int, input().split(" "))
+  lines[u].append( (v,w) )
+  
+distance = [INF] * (V+1)
+visited = [0] * (V+1)
+
+visited[k] = 1
+distance[k] = 0
+# 처음 distance 초기화
+for i in lines[k]:
+  distance[i[0]] = i[1]
+
+
+def find_nearest_node():
+  global distance
+  global V
+  min_value = INF
+  min_index = 0
+  for i in range(1, V+1):
+    if( distance[i] < min_value and visited[i]==0 ):
+      min_index = i
+      min_value = distance[i]
+  return min_index
+
+def dijkstra():
+  global distance
+  global visited
+  global lines
+  global V
+
+  
+  for _ in range(V+1):
+    nearest_node = find_nearest_node()
+    visited[nearest_node] = 1
+    
+    for i in lines[nearest_node]:
+      dist = distance[ nearest_node ] + i[1]
+      if( dist < distance[i[0]] ):
+        distance[i[0]] = dist
+        visited[i[0]] = 1
+
+dijkstra()
+
+for i in range(1, len(distance)):
+  if( distance[i]==INF ):
+    print("INF")
+  else:
+    print(distance[i])
