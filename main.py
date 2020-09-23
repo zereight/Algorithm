778588dea6e45670e7c68c448d9339493be69827
fullstackmachin <fullstackmachinelearning@gmail.com>
2020-09-23 15:14:18 +0000

    1238 파티 (PASS)

--- main.py
+++ main.py
@@ -1,72 +1,90 @@
 '''
 문제
-방향그래프가 주어지면 주어진 시작점에서 다른 모든 정점으로의 최단 경로를 구하는 프로그램을 작성하시오. 단, 모든 간선의 가중치는 10 이하의 자연수이다.
+N개의 숫자로 구분된 각각의 마을에 한 명의 학생이 살고 있다.
+
+어느 날 이 N명의 학생이 X (1 ≤ X ≤ N)번 마을에 모여서 파티를 벌이기로 했다. 
+이 마을 사이에는 총 M개의 단방향 도로들이 있고 i번째 길을 지나는데 Ti(1 ≤ Ti ≤ 100)의 시간을 소비한다.
+
+각각의 학생들은 파티에 참석하기 위해 걸어가서 다시 그들의 마을로 돌아와야 한다. 하지만 이 학생들은 워낙 게을러서 최단 시간에 오고 가기를 원한다.
+
+이 도로들은 단방향이기 때문에 아마 그들이 오고 가는 길이 다를지도 모른다. N명의 학생들 중 오고 가는데 가장 많은 시간을 소비하는 학생은 누구일지 구하여라.
 
 입력
-첫째 줄에 정점의 개수 V와 간선의 개수 E가 주어진다. (1≤V≤20,000, 1≤E≤300,000) 모든 정점에는 1부터 V까지 번호가 매겨져 있다고 가정한다. 둘째 줄에는 시작 정점의 번호 K(1≤K≤V)가 주어진다. 셋째 줄부터 E개의 줄에 걸쳐 각 간선을 나타내는 세 개의 정수 (u, v, w)가 순서대로 주어진다. 이는 u에서 v로 가는 가중치 w인 간선이 존재한다는 뜻이다. u와 v는 서로 다르며 w는 10 이하의 자연수이다. 서로 다른 두 정점 사이에 여러 개의 간선이 존재할 수도 있음에 유의한다.
+첫째 줄에 N(1 ≤ N ≤ 1,000), M(1 ≤ M ≤ 10,000), X가 공백으로 구분되어 입력된다. 두 번째 줄부터 M+1번째 줄까지 i번째 도로의 시작점, 끝점, 그리고 이 도로를 지나는데 필요한 소요시간 Ti가 들어온다. 시작점과 끝점이 같은 도로는 없으며, 시작점과 한 도시 A에서 다른 도시 B로 가는 도로의 개수는 최대 1개이다.
+
+모든 학생들은 집에서 X에 갈수 있고, X에서 집으로 돌아올 수 있는 데이터만 입력으로 주어진다.
 
 출력
-첫째 줄부터 V개의 줄에 걸쳐, i번째 줄에 i번 정점으로의 최단 경로의 경로값을 출력한다. 시작점 자신은 0으로 출력하고, 경로가 존재하지 않는 경우에는 INF를 출력하면 된다.
+첫 번째 줄에 N명의 학생들 중 오고 가는데 가장 오래 걸리는 학생의 소요시간을 출력한다.
 
-5 6
-1
-5 1 1
-1 2 2
-1 3 3
-2 3 4
-2 4 5
-3 4 6
+4 8 2
+1 2 4
+1 3 2
+1 4 7
+2 1 1
+2 3 5
+3 1 2
+3 4 4
+4 2 3
 '''
 import sys
 import heapq
 input = sys.stdin.readline
-V, E = map(int, input().split(" "))
-
-start = int(input())
-
+N, M, X = map(int, input().split(" "))
 lines = dict()
-for i in range(V+1):
+for i in range(N+1):
   lines[i] = []
 
-for _ in range(E):
-  u,v,w = map(int, input().split(" "))
-  lines[u].append( (v,w) )
+for _ in range(M):
+  start,end,weight = map(int, input().split(" "))
+  lines[start].append( (end, weight) )
 
-visited = [False]*(V+1)
-INF = int(1e9)
+# 각각의 모든 노드가 시작점일때의 다익스트라 distance
+all_distances = dict()
+for i in range(N+1):
+  all_distances[i] = None
 
-# start에서 i까지의 거리
-distance = [INF] * (V+1)
-
-def dijkstra(start):
-  global visited
-  global distance
+def dijkstra():
+  global N
+  global all_distances
   global lines
+  global X
+  INF = int(1e9)
   
-  # 첫 distance 초기화
-  distance[start] = 0
 
-  q = []
-  # 처음 우선 순위큐에, (거리, 시작지점)넣기
-  # 자기자신과의 거리는 0이고 낮을 수록 우선순위 큐의 우선이 되기떄문에 순서 바꿈
-  heapq.heappush(q, (0, start))
+  for start in range(1,N+1):
+    distance = [INF] * (N+1)
+    distance[start] = 0
+    visited = [False] * (N+1)
+    q = [] # [(weight, destination)]
 
-  while len(q) > 0:
-    smallest_dist_cost, smallest_dist_node = heapq.heappop(q)
-    # 방문처리
-    visited[smallest_dist_node] = True
-    for node in lines[smallest_dist_node]:
-      dest, weight = node
-      if( visited[dest] == True ):
-        continue
-      if( distance[dest] > distance[smallest_dist_node] + weight ):
-        distance[dest] = distance[smallest_dist_node] + weight
-        heapq.heappush(q, (distance[dest], dest))
+    heapq.heappush(q, (0, start))
 
-dijkstra(start)
+    while( len(q) > 0 ):
+      smallest_dist, smallest_node = heapq.heappop(q)
+      visited[smallest_node] = True # 방문처리
+      for node in lines[smallest_node]:
+        dest, weight = node
 
-for i in range(1, len(distance)):
-  if( distance[i] == INF ):
-    print("INF")
-  else:
-    print(distance[i])
+        if( visited[dest] == True ): 
+          continue
+        
+        if( (smallest_dist + weight) < distance[dest]):
+          distance[dest] = smallest_dist + weight
+          heapq.heappush(q, (distance[dest], dest))
+
+
+    all_distances[start] = list(distance)
+
+  # X 입장에서 모든 경로로 가는 최단거리
+  ans = []
+  for i in range(1, N+1):
+    
+    a = all_distances[i][X] # i입장에서 X까지 최단거리
+    b = all_distances[X][i] # X입장에서 i까지 최단거리
+    ans.append(a+b)
+
+  return max(ans)
+
+
+print(dijkstra())
