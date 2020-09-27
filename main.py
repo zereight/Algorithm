a63b671cb071fc9f86fd9c0eef16cc09642763ab
zereight <42544600+zereight@users.noreply.github.com>
2020-09-28 02:12:59 +0900

    11403 경로찾기 Pass
    플로이드 워셜 알고리즘

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
-import sys input = sys.stdin.readline n = int(input()) cardPack = dict() for i,v in enumerate(map(int, input().rstrip().split(" "))): cardPack[i+1] = v # dp[i]는 i개의 카드를 산다했을때 가장 많이 지불할 수 있는 가격 dp = [0] * (n+1) dp[1] = cardPack[1] for i in range(1, n+1): for j in range(i,0,-1): dp[i] = max([ dp[i], cardPack[i], dp[i-j] + cardPack[j] ]) print(dp[n])
+'''
+문제
+가중치 없는 방향 그래프 G가 주어졌을 때, 모든 정점 (i, j)에 대해서, i에서 j로 가는 경로가 있는지 없는지 구하는 프로그램을 작성하시오.
 
-출처: https://zereight.tistory.com/633 [Zereight's Blog]
+입력
+첫째 줄에 정점의 개수 N (1 ≤ N ≤ 100)이 주어진다. 둘째 줄부터 N개 줄에는 그래프의 인접 행렬이 주어진다. i번째 줄의 j번째 숫자가 1인 경우에는 i에서 j로 가는 간선이 존재한다는 뜻이고, 0인 경우는 없다는 뜻이다. i번째 줄의 i번째 숫자는 항상 0이다.
+
+출력
+총 N개의 줄에 걸쳐서 문제의 정답을 인접행렬 형식으로 출력한다. 정점 i에서 j로 가는 경로가 있으면 i번째 줄의 j번째 숫자를 1로, 없으면 0으로 출력해야 한다.
+'''
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+graph = []
+answer = []
+for _ in range(n):
+  graph.append( list(map(int,input().rstrip().split(" ") )))
+
+for k in range(n):
+  for i in range(n):
+    for j in range(n):
+      if(graph[i][k]+graph[k][j] == 2 or graph[i][j] != 0):
+        graph[i][j] = 1
+
+# for i in range(n):
+#   answer[i][i] = 1
+
+for i in graph:
+  print(" ".join( map(str,i) ))
