ba5a925fce8eb99a2ba00837d293f2adfc96b895
zereight <42544600+zereight@users.noreply.github.com>
2020-09-29 22:48:07 +0900

    11404 플로이드 PASS
    갈수 없을때 0을 출력하는것이 히든테케

--- main.py
+++ main.py
@@ -1,40 +1,24 @@
 import sys
 input = sys.stdin.readline
 
-n,m = map(int, input().rstrip().split(" "))
+n = int(input().rstrip())
+m = int(input().rstrip())
 INF = int(1e9)
-graph = [ [INF] * (n+1) for _ in range(n+1) ]
+graph = [ [INF]*(n+1) for _ in range(n+1) ]
 for _ in range(m):
-  a,b = list(map(int,input().rstrip().split(" ") ))
-  
-  graph[a][b] = 1
-  graph[b][a] = 1
+    start, end, weight = map(int, input().rstrip().split(" "))
+    graph[start][end] = min(weight,graph[start][end])
 
+for k in range(1,n+1):
+    for i in range(1,n+1):
+        for j in range(1,n+1):
+            graph[i][j] = min( graph[i][j], graph[i][k]+graph[k][j] )
 
-for k in range(1, n+1):
-  for i in range(1, n+1):
-    for j in range(1, n+1):
-      graph[i][j] = min([ graph[i][j], graph[i][k]+graph[k][j] ])
+for i in range(n+1):
+    graph[i][i] = 0
+    for j in range(n+1):
+        if(graph[i][j] == INF):
+            graph[i][j] = 0
 
-
-# for i in range(1,n+1):
-#   for j in range(1, n+1):
-#     if(i==j):
-#         graph[i][j] = 0
-#     if(graph[i][j] == INF):
-#       graph[i][j] = 0
-#   print(" ".join( map(str, graph[i][1:]) ) )
-# for i in range(1,n+1):
-#   print(sum(graph[i][1:]))
-
-ans = []
-for i in range(1, n+1):
-  tmp = 0
-  for j in range(1, n+1):
-    if(i==j):
-      graph[i][j] = 0
-    if( graph[i][j] != INF ):
-      tmp += graph[i][j]
-  ans.append( (i, tmp) )
-
-print( sorted(ans, key=lambda x:(-x[1], -x[0]))[-1][0] )
+for i in graph[1:]:
+    print(" ".join(map(str,i[1:])))
