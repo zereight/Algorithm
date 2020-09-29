3dc4b11da5005c5836a121fdf684757f32e67115
zereight <42544600+zereight@users.noreply.github.com>
2020-09-30 01:00:17 +0900

    2665 미로만들기 PASS

--- main.py
+++ main.py
@@ -1,24 +1,47 @@
 import sys
 input = sys.stdin.readline
 
+from collections import deque
+
 n = int(input().rstrip())
-m = int(input().rstrip())
+board = [] # 미로
+dp = [] # 흰방으로 바꾼 개수 누적
 INF = int(1e9)
-graph = [ [INF]*(n+1) for _ in range(n+1) ]
-for _ in range(m):
-    start, end, weight = map(int, input().rstrip().split(" "))
-    graph[start][end] = min(weight,graph[start][end])
+for _ in range(n):
+    board.append( list(map(int,list( input().rstrip() ) ) ) )
+    dp.append( [INF]*n )
+    
 
-for k in range(1,n+1):
-    for i in range(1,n+1):
-        for j in range(1,n+1):
-            graph[i][j] = min( graph[i][j], graph[i][k]+graph[k][j] )
+def bfs():
+    global n, board, visited, dp
+    # 상하좌우
+    x_direction=[-1,1,0,0]
+    y_direction=[0,0,-1,1]
+    
+    q = deque()
+    q.append((0,0))
+    dp[0][0]=0
+    
+    while(len(q) != 0):
+        
+        x,y = q.popleft()
 
-for i in range(n+1):
-    graph[i][i] = 0
-    for j in range(n+1):
-        if(graph[i][j] == INF):
-            graph[i][j] = 0
-
-for i in graph[1:]:
-    print(" ".join(map(str,i[1:])))
+        for i in range(4):
+            new_x, new_y = x+x_direction[i], y+y_direction[i]
+            
+            if( new_x >= 0 and new_y>=0 and new_x<n and new_y<n ): # 유효범위 내에 있는지 체크
+                
+                if( board[new_x][new_y] == 0 ): # 검은벽
+                  if(dp[new_x][new_y] > dp[x][y]+1): # 더 큰값이면 이동해서 작게 만들어준다.
+                    dp[new_x][new_y] = dp[x][y]+1
+                    q.append((new_x, new_y))
+                else: # 하얀벽
+                    if(dp[new_x][new_y] > dp[x][y]): # 더 큰값이면 이동해서 작게 만들어준다.
+                        dp[new_x][new_y] = dp[x][y]
+                        q.append((new_x, new_y))
+    
+bfs()
+print(dp[n-1][n-1])
+# for i in dp:
+#   print(" ".join(map(str,i)))
+              
