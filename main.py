bf9e73d05f27de0fe14c609bae840d35db704520
zereight <42544600+zereight@users.noreply.github.com>
2020-10-02 12:27:27 +0900

    11047 동전 0 PASS

--- main.py
+++ main.py
@@ -1,51 +1,17 @@
 import sys
 input = sys.stdin.readline
 
-import heapq
+n,k = map(int, input().rstrip().split(" "))
 
-T = int(input())
+coins = []
+for _ in range(n):
+  coins.append( int(input().rstrip()) )
 
-INF = int(1e9)
+cursor = n-1
+answer = 0
+while(k!=0):
+  answer += k//coins[cursor]
+  k -= coins[cursor] * (k//coins[cursor])
+  cursor-=1
 
-def countHacked(arr):
-  return len(list(filter(lambda x: x!=-1, arr)))
-
-answer = []
-
-for _ in range(T):
-  n,d,start = map(int, input().rstrip().split(" "))
-  info = dict()
-  visited = [0] * (1+n)
-  distance = [INF] * (1+n)
-
-  for i in range(1, n+1):
-    info[i] = []
-  
-  for __ in range(d):
-    b,a,s = map(int, input().rstrip().split(" "))
-    info[a].append( (b,s) )
-  
-
-  q = []
-  heapq.heappush(q, (0, start))
-  distance[start] = 0
-  
-  while( len(q) != 0 ):
-    weight, curr = heapq.heappop(q)
-    visited[curr] = 1
-    
-    for i in info[curr]:
-      dest, cost = i
-      if( visited[dest] == 0 ):
-        if( distance[dest] > weight + cost):
-          distance[dest] = weight+cost
-          heapq.heappush(q, (distance[dest], dest))
-  
-  for i in range(len(distance)):
-    if(distance[i] == INF):
-      distance[i] = -1
-  
-  answer.append( (countHacked(distance), max(distance) ))
-
-for i in answer:
-  print(" ".join(map(str, i)))
+print(answer)
