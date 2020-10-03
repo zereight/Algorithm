0dde955d4e5d93a139b0264bf7ad71b09ef2e525
zereight <42544600+zereight@users.noreply.github.com>
2020-10-04 00:59:25 +0900

    10162 전자레인지

--- main.py
+++ main.py
@@ -3,29 +3,14 @@ input = sys.stdin.readline
 
 T = int(input().rstrip())
 
-def solve(man):
-  
-  man = sorted(man, key=lambda x: x[0]) # 서류순위 순으로 정렬
-  min_score = man[0][1] # 면접 순위를 기준으로 체크할것임
-  count = 0
-  for m in man:
-    if( m[1] <= min_score ):
-      count += 1
-      min_score = m[1]
-  
-  return count
-    
+btns = [300,60,10]
+counts = [0,0,0]
 
-answer = []
-for _ in range(T):
-  n = int(input().rstrip())
+for i,btn in enumerate(btns):
+  counts[i] += T//btn
+  T = T%btn
 
-  man = []
-  for _ in range(n):
-    # 동석차 없음, 순위가 낮을 수록 좋은거임
-    man.append( list(map(int, input().rstrip().split(" ")) ) )
-  
-  answer.append( solve(man) )
-
-for a in answer:
-  print(a)
+if(T==0):
+  print(" ".join(map(str,counts)))
+else:
+  print(-1)
