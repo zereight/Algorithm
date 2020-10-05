7031934527e04b1e78deba6895a0443f365d2296
zereight <42544600+zereight@users.noreply.github.com>
2020-10-05 19:49:46 +0900

    1197 최소 스패닝 트리 PASS

--- main.py
+++ main.py
@@ -1,19 +1,43 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
+v,e = map(int, input().rstrip().split(" "))
+graph = []
+for _ in range(e):
+  a,b,c = map(int, input().rstrip().split(" "))
+  graph.append( (a,b,c) )
 
-dp = []
-for _ in range(n+1):
-  dp.append( [0]*(11) )
+graph = sorted(graph, key= lambda x:x[2])
 
-# dp[i][j] i길이의 숫자에서 맨왼쪽숫자가 j일때 개수
+# 부모노드 저장 리스트
+parents = [0] * (v+1)
 
-for i in range(1,11):
-  dp[1][i] = 1
+# parent 를 자기자신으로 설정
+for i in range(v+1):
+  parents[i] = i
 
-for i in range(2,n+1):
-  for j in range(1,11):
-    dp[i][j] = sum(dp[i-1]) - sum(dp[i-1][:j])
+# 최종 루트노드를 찾는 함수
+def find_parent(parent, x):
+  if(parent[x] != x):
+    parent[x] = find_parent(parent, parent[x])
+  return parent[x]
 
-print(sum(dp[n])%10007)
+# union, 숫자가 더 적은것을 부모처리
+def union(parent, a, b):
+  a = find_parent(parent, a)
+  b= find_parent(parent,b)
+  if(a<b):
+    parent[b] = a
+  else:
+    parent[a] = b
+
+
+answer = 0
+for edge in graph:
+  a,b,c = edge
+  # 최종 부모가 다르면
+  if(find_parent(parents,a) != find_parent(parents,b)):
+    union(parents, a, b) # union
+    answer+=c
+
+print(answer)
