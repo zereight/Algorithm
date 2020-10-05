02b86f3de4b0d0f8ed79f123cfa2b34119838146
zereight <42544600+zereight@users.noreply.github.com>
2020-10-05 20:16:51 +0900

    1647 도시분한계획 PASS

--- main.py
+++ main.py
@@ -1,43 +1,45 @@
 import sys
 input = sys.stdin.readline
 
-v,e = map(int, input().rstrip().split(" "))
+n,m = map(int, input().rstrip().split(" "))
+
 graph = []
-for _ in range(e):
+for _ in range(m):
   a,b,c = map(int, input().rstrip().split(" "))
   graph.append( (a,b,c) )
 
-graph = sorted(graph, key= lambda x:x[2])
+parents = [0] * (n+1)
 
-# 부모노드 저장 리스트
-parents = [0] * (v+1)
-
-# parent 를 자기자신으로 설정
-for i in range(v+1):
+# 자기자신으로 부모노드 초기화
+for i in range(n+1):
   parents[i] = i
 
-# 최종 루트노드를 찾는 함수
-def find_parent(parent, x):
-  if(parent[x] != x):
-    parent[x] = find_parent(parent, parent[x])
-  return parent[x]
+# 가중치 기반 정렬
+graph = sorted(graph, key=lambda x:x[2])
 
-# union, 숫자가 더 적은것을 부모처리
-def union(parent, a, b):
-  a = find_parent(parent, a)
-  b= find_parent(parent,b)
+# 최종 루트노드를 찾기
+def find_root_node(parents, x):
+  if( parents[x] != x ):
+    parents[x] = find_root_node(parents, parents[x])
+  return parents[x]
+
+# Union
+def union(parents, a,b):
+  a=find_root_node(parents,a)
+  b=find_root_node(parents,b)
   if(a<b):
-    parent[b] = a
+    parents[b] = a
   else:
-    parent[a] = b
-
-
+    parents[a] = b
+  
 answer = 0
+# 최소 스패닝 트리만든다음에 스패닝트리에서 가장 가중치 큰 간선빼면 2개의 마을로 분할됨
+maxium_edge_weight_in_spanninTree = 0
 for edge in graph:
   a,b,c = edge
-  # 최종 부모가 다르면
-  if(find_parent(parents,a) != find_parent(parents,b)):
-    union(parents, a, b) # union
+  if(find_root_node(parents,a) != find_root_node(parents,b)):
+    union(parents, a,b)
+    if( maxium_edge_weight_in_spanninTree < c):
+      maxium_edge_weight_in_spanninTree=c
     answer+=c
-
-print(answer)
+print(answer - maxium_edge_weight_in_spanninTree)
