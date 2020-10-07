8a125b8b5b4140ddaeddf274b3b9cc2bc43537a4
zereight <42544600+zereight@users.noreply.github.com>
2020-10-07 22:42:27 +0900

    2512 예산 binary search upper

--- main.py
+++ main.py
@@ -2,19 +2,34 @@ import sys
 input = sys.stdin.readline
 
 n = int(input().rstrip())
-k = int(input().rstrip())
+req = list(map(int, input().rstrip().split(" ")))
+total = int(input().rstrip())
 
-points = list(map(int, input().rstrip().split(" ")))
+req = sorted(req)
 
-# 정렬
-points = sorted(list(set(points)))
 
-gap = []
-if(len(points) > 1):
-    for i in range(0, len(points)-1):
-        gap.append(abs(points[i]-points[i+1]))
-    gap = sorted(gap)
-    for i in range(k-1):
-        gap.pop()
+def getTotal(req, x):
+    ans = 0
+    for r in req:
+        if(r <= x):
+            ans += r
+        else:
+            ans += x
+    return ans
 
-print(sum(gap))
+
+def binary_search_upper(req, start, end, target):
+    while(start <= end):
+        mid = (start+end)//2
+        # 배정한 예산의 합이 총예산 이하이면 1더 더해서 최대 m을 찾아준다.
+        if(getTotal(req, mid) <= target):
+            start = mid+1
+
+        else:
+            end = mid-1
+
+    return end
+
+
+answer = binary_search_upper(req, 0, max(req), total)
+print(answer)
