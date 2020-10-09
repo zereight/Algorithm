d8c51e37bbc1b68c89def531533c67a4a11bb851
zereight <42544600+zereight@users.noreply.github.com>
2020-10-09 18:25:17 +0900

    1516 게임개발 PASS

--- main.py
+++ main.py
@@ -1,35 +1,44 @@
 import sys
-import heapq
+from collections import deque
 input = sys.stdin.readline
 
-n, m = map(int, input().rstrip().split(" "))
+n = int(input().rstrip())
+
 graph = dict()
 for i in range(1, n+1):
     graph[i] = []
 inDegree = [0]*(n+1)
-inDegree[0] = -1  # 1부터 시작
-for _ in range(m):
-    start, end = map(int, input().rstrip().split(" "))
-    graph[start].append(end)
-    inDegree[end] += 1
 
+costs = [0]*(n+1)
+for i in range(1, n+1):
+    info = list(map(int, input().rstrip().split(" ")))[:-1]
+    costs[i] = info[0]
+    must_list = info[1:]
 
-q = []
-for i in range(n+1):
-    if(inDegree[i] == 0):
-        heapq.heappush(q, i)
-        inDegree[i] -= 1
+    for j in must_list:
+        graph[j].append(i)
+        inDegree[i] += 1  # InDegree 증가
 
-ans = []
-while(len(q) != 0):
-    i = heapq.heappop(q)
-    ans.append(i)
+spendTime = [0]*(n+1)
+while(1):
+    q = deque()
+    for i in range(1, n+1):  # indegree 0인거 추가
+        if(inDegree[i] == 0):
+            q.append(i)
+            inDegree[i] -= 1
 
-    for j in graph[i]:
-        inDegree[j] -= 1
-        if(inDegree[j] == 0):
-            heapq.heappush(q, j)
-            inDegree[j] -= 1
+    if(len(q) == 0):
+        break
 
+    # InDegree 0인 건물에 한해서 동시 건축
+    while(len(q) != 0):
+        building = q.pop()
+        spendTime[building] += costs[building]
 
-print(" ".join(map(str, ans)))
+        # 연결된 간선에 indegree - 1
+        for g in graph[building]:
+            inDegree[g] -= 1
+            spendTime[g] = max(spendTime[g], spendTime[building])
+
+for i in spendTime[1:]:
+    print(i)
