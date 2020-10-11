50d2bdf1f2c4eb604d11966b66cd5fdd3365ae91
zereight <42544600+zereight@users.noreply.github.com>
2020-10-11 22:41:44 +0900

    2014 소수의 곱 (우선순위 큐)

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import sys
 import heapq
 input = sys.stdin.readline
-n = int(input().rstrip())
-q = []
+
+k, n = map(int, input().rstrip().split(" "))
+
+arr = list(map(int, input().rstrip().split(" ")))
+q = list(arr)
+heapq.heapify(q)
+head = None
 for _ in range(n):
-    q.extend(list(map(int, input().rstrip().split(" "))))
-    q = heapq.nlargest(n, q)
-print(q[-1])
+    head = heapq.heappop(q)
+    for a in arr:
+        heapq.heappush(q, head*a)
+        if(head % a == 0):
+            break
+
+print(head)
