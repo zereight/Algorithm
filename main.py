4d22cf243f4b8f8fa384d6983ddb97b1abec0959
zereight <42544600+zereight@users.noreply.github.com>
2020-10-11 12:40:25 +0900

    1927 최소힙, 11279 최대힙 우선순위큐, 힙

--- main.py
+++ main.py
@@ -1,59 +1,46 @@
+# 최소힙
+import heapq
 import sys
-from collections import deque
 input = sys.stdin.readline
 
+q = []
 
-def bfs(board, visited, row, col, marking):
-    w, h = len(board[0]), len(board)
-    # 상,하,좌,우, 좌상, 좌하, 우상, 우하
-    direction_x = [-1, 1, 0, 0, -1, 1, -1, 1]
-    direction_y = [0, 0, -1, 1, -1, -1, 1, 1]
+n = int(input().rstrip())
+answer = []
+for _ in range(n):
+    x = int(input().rstrip())
 
-    q = deque()
+    if(x == 0):
+        if(len(q) == 0):
+            answer.append(0)
+        else:
+            answer.append(heapq.heappop(q))
+    else:
+        heapq.heappush(q, x)
 
-    q.append((row, col))
-    while(len(q) != 0):
-
-        x, y = q.pop()
-        visited[x][y] = marking
-        for i in range(8):
-            new_x, new_y = x+direction_x[i], y+direction_y[i]
-
-            # 유효범위인가
-            if(new_x >= 0 and new_y >= 0 and new_x < h and new_y < w):
-                # 육지이고, 방문하지 않았는가
-                if(board[new_x][new_y] == 1 and visited[new_x][new_y] == 0):
-                    q.append((new_x, new_y))
+for a in answer:
+    print(a)
 
 
-answers = []
-while(1):
-    w, h = map(int, input().rstrip().split(" "))
+# 최대힙
+import heapq
+import sys
+input = sys.stdin.readline
 
-    if(w == 0 and h == 0):
-        break
+q = []
 
-    board = []
-    for _ in range(h):
-        board.append(list(map(int, input().rstrip().split(" "))))
+n = int(input().rstrip())
+answer = []
+for _ in range(n):
+    x = int(input().rstrip())
 
-    visited = [[0]*w for _ in range(h)]
+    if(x == 0):
+        if(len(q) == 0):
+            answer.append(0)
+        else:
+            answer.append(-1*heapq.heappop(q))
+    else:
+        heapq.heappush(q, -1*x)
 
-    # visited에서 같은 육지에서는 같은 marking값을 가지게 해줌
-    marking = 1
-    for i in range(h):
-        for j in range(w):
-            if(board[i][j] == 1 and visited[i][j] == 0):
-                bfs(board, visited, i, j, marking)
-                marking += 1
-
-    flat_board = []
-    for b in visited:
-        flat_board.extend(b)
-
-    if(0 in flat_board):  # 0이 포함되어있다면 -1빼줌, visited에서 0은 방문하지 않은 것이므로,
-        answers.append(len(set(flat_board)) - 1)
-    else:  # 0이 없는경우는 다 방문한것.
-        answers.append(len(set(flat_board)))
-for a in answers:
+for a in answer:
     print(a)
