fbd3e50e66ceaef64a58a9705df37d355f358dbd
zereight <42544600+zereight@users.noreply.github.com>
2020-10-11 18:55:17 +0900

    1715 카드 정렬하기 우선순위큐

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 import sys
-import bisect
+import heapq
 input = sys.stdin.readline
 
 n = int(input().rstrip())
-x = []
-answers = []
-for _ in range(n):
-    bisect.insort(x, int(input().rstrip()))
-    if(len(x) % 2 == 0):
-        answers.append(x[int(len(x)/2)-1])
-    else:
-        answers.append(x[int(len(x)/2)])
 
-for a in answers:
-    print(a)
+cards = []
+
+# 카드 입력받기
+for _ in range(n):
+    heapq.heappush(cards, int(input().rstrip()))
+
+answer = 0
+while(len(cards) != 0):
+    # 제일 작은값 1개 뺀다
+    a = heapq.heappop(cards)
+    # 다 뺀거면 중단
+    if(len(cards) == 0):
+
+        break
+    # 2번째꺼 뺀다.
+    b = heapq.heappop(cards)
+
+    # 더한것을 큐에 다시 넣는다.
+    answer += (a+b)
+    heapq.heappush(cards, a+b)
+
+print(answer)
