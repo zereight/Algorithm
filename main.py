3914a723ea71ca2f801747b488363ca9aa5e0492
zereight <42544600+zereight@users.noreply.github.com>
2020-10-12 11:54:13 +0900

    2217 로프 (그리디)

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import sys
 input = sys.stdin.readline
 
-coins = [500, 100, 50, 10, 5, 1]
+n = int(input().rstrip())
 
-money = 1000-int(input().rstrip())
+# 모든 로프를 사용할 필요가 없다.
+ropes = []
+for _ in range(n):
+    ropes.append(int(input().rstrip()))
 
-count = 0
+# 역순정렬
+ropes = sorted(ropes, reverse=True)
+m = 0
+for i in range(n):
+    # 1개선택 or 지금까지 로프만 선택
+    m = max(m, ropes[i]*(i+1))
 
-for coin in coins:
-    count += money//coin
-    money = money % coin
-
-print(count)
+print(m)
