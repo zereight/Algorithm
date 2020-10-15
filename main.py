1ce23d29fcd226544f6358d952f4d8520850bf3a
zereight <42544600+zereight@users.noreply.github.com>
2020-10-16 02:00:23 +0900

    1759 암호만들기 (브루트포스, 문자열)

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 import sys
-from itertools import combinations
+from itertools import combinations, product
 input = sys.stdin.readline
 
-n, m = map(int, input().rstrip().split(" "))
-cards = list(map(int, input().rstrip().split(" ")))
+L, C = map(int, input().rstrip().split(" "))
+s = input().rstrip().split(" ")
 
-print(
-    sum(
-        sorted(
-            filter(lambda x: sum(x) <= m, combinations(cards, 3)),
-            key=lambda x: (abs(sum(x)-m), sum(x)-m)
-        )[0]
-    )
-)
+moem = []
+jaem = []
+
+for _s in s:
+    if(_s in ['a', 'e', 'i', 'o', 'u']):
+        moem.append(_s)
+    else:
+        jaem.append(_s)
+
+ans = set()
+for i in range(L+1):  # 모음최소 1개부터 시작
+    moem_len = i
+    jaem_len = L-i
+
+    if(moem_len >= 1 and jaem_len >= 2):
+        moem_comb = list(combinations(moem, moem_len))
+        jaem_comb = list(combinations(jaem, jaem_len))
+        for m in moem_comb:
+            for j in jaem_comb:
+                ans.add(''.join(sorted(list("".join(m)+"".join(j)))))
+
+ans = sorted(list(ans))
+for a in ans:
+    print(a)
