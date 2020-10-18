58b132769d5b8ab69c0ed2336c1e5438ee92ebb9
zereight <42544600+zereight@users.noreply.github.com>
2020-10-19 02:14:32 +0900

    18352 특정한 거리의 도시 찾기 (BFS)

--- main.py
+++ main.py
@@ -1,71 +1,40 @@
 import sys
 from collections import deque
+import bisect
 input = sys.stdin.readline
 
-n = int(input().rstrip())
-k = int(input().rstrip())
-apples = []
-for _ in range(k):
-    x, y = map(int, input().rstrip().split(" "))
-    apples.append((x-1, y-1))
+n, m, k, x = map(int, input().rstrip().split(" "))
+graph = dict()
+for i in range(1, n+1):
+    graph[i] = []
+for _ in range(m):
+    start, end = map(int, input().rstrip().split(" "))
+    graph[start].append(end)
 
-L = int(input().rstrip())
-info = deque()
-for _ in range(L):
-    t, direction = input().rstrip().split(" ")
-    info.append((int(t), direction))
+visited = [-1]*(n+1)
 
-curr_x, curr_y = 0, 0
+curr = x
+q = deque()
+q.append(curr)
+distance = 1
+visited[curr] = 0
+answer = []
 
-heading = 0  # 0:우, 1:하, 2:좌, 3:상
-
-body = deque()
-
-
-def turn_head(direction):
-    global heading
-    if(direction == "L"):  # 좌회전
-        heading = (heading-1) % 4
-    elif(direction == "D"):
-        heading = (heading+1) % 4
-
-
-def go(x, y):
-    if(heading == 0):  # 오른쪽
-        return (x, y+1)
-    elif(heading == 1):  # 하단
-        return (x+1, y)
-    elif(heading == 2):  # 왼쪽
-        return (x, y-1)
-    elif(heading == 3):  # 상단
-        return (x-1, y)
-
-
-def isValid(x, y):
-    global n, body
-    if(x >= 0 and y >= 0 and x < n and y < n):
-        if((x, y) not in body):
-            return True
-    return False
-
-
-body.append((0, 0))
-
-i = 0
-while(1):
-    i += 1
-    curr_x, curr_y = go(curr_x, curr_y)  # 간다.
-    if(not isValid(curr_x, curr_y)):  # 유효하지 않으면 취소
-        print(i)
+while(len(q) != 0):
+    curr = q.popleft()
+    if(visited[curr] == k):
         break
-    if((curr_x, curr_y) not in apples):  # 사과 좌표가 아니라면
-        body.popleft()  # 몸통1개 뺸다.
-    else:  # 사과 좌표라면 사과제거해준다!!!!!
-        apples.remove((curr_x, curr_y))
-    # 유효했다면 몸통추가
-    body.append((curr_x, curr_y))
+    # print(curr)
+    for dest in graph[curr]:
+        if(visited[dest] == -1):
+            q.append(dest)
+            visited[dest] = visited[curr]+1
+            if(visited[curr]+1 == k):
+                bisect.insort(answer, dest)
 
-    if(len(info) != 0):
-        if(i == info[0][0]):  # 해당 시간이 되면
-            t, direction = info.popleft()
-            turn_head(direction)  # 회전
+
+if(len(answer) != 0):
+    for i in answer:
+        print(i)
+else:
+    print(-1)
