1810d0e86c4275bee4e28240c6925799a0c4bb80
zereight <42544600+zereight@users.noreply.github.com>
2020-10-21 06:43:32 +0900

    17142 연구소2

--- main.py
+++ main.py
@@ -1,34 +1,87 @@
 import sys
 import copy
+from collections import deque
 from itertools import combinations
-input = sys.stdin.readline
+sys.stdin.readline
 
 n, m = map(int, input().rstrip().split(" "))
 
-chicken_pos = []
-house_pos = []
+board = []
+virus_pos = []
+zero_count = 0
 for i in range(n):
-    row = list(map(int, input().rstrip().split(" ")))
+    temp = list(map(int, input().rstrip().split(" ")))
     for j in range(n):
-        if(row[j] == 2):
-            chicken_pos.append((i, j))
-        elif(row[j] == 1):
-            house_pos.append((i, j))
+        if(temp[j] == 2):
+            virus_pos.append((i, j))
+            temp[j] = 0  # 바이러스 있는곳도 랜덤으로 위치시켜줄거니까 일단 0
+            zero_count += 1  # 빈공간 몇개인지 세주기
+        elif(temp[j] == 0):
+            zero_count += 1  # 빈공간 몇개인지 세주기
+    board.append(temp)
 
 
-def getChickenDistance(a, b):
-    return abs(a[0]-b[0]) + abs(a[1]-b[1])
+def bfs(n, board, virus_lists, zero_count):
+    # 상,하,좌,우
+    direction = [
+        (-1, 0), (1, 0), (0, -1), (0, 1)
+    ]
+
+    newBoard = copy.deepcopy(board)
+    visited = [[-1]*n for _ in range(n)]
+    q = deque()
+    count = 0
+    for v in virus_lists:  # 바이러스 먼저 큐에 넣고, 방문처리
+        q.append(v)
+        visited[v[0]][v[1]] = count
+
+    max_time = 0
+    available_space = zero_count - len(virus_lists)  # 현재 남은 빈공간개수
+
+    while(len(q) != 0):
+        x, y = q.popleft()
+
+        for i in range(4):
+            new_x, new_y = x+direction[i][0], y+direction[i][1]
+
+            if(new_x >= 0 and new_y >= 0 and new_x < n and new_y < n):
+                if(newBoard[new_x][new_y] == 0 and visited[new_x][new_y] == -1):
+                    q.append((new_x, new_y))
+                    visited[new_x][new_y] = visited[x][y]+1  # 카운트
+                    max_time = max(max_time, visited[new_x][new_y])
+                    available_space -= 1  # 빈공간 1개 삭제
+
+                    if(available_space <= 0):  # 빈공간이 0이 되었으면
+                        # print()
+                        # for v in visited:
+                        #     print(v)
+                        return max_time
+    # print()
+    # for v in visited:
+    #     print(v)
+    # print(available_space)
+    if(available_space <= 0):
+        '''
+        예제 히든테케 통과용
+        5 2
+        1 1 1 1 1
+        1 1 1 1 1
+        1 1 1 1 1
+        1 1 1 1 1
+        1 1 2 2 1
+        '''
+        return max_time
+
+    return -1  # 빈공간0에 도달하지 못했음
 
 
 answer = float('inf')
-for s in combinations(chicken_pos, m):  # 살릴 치킨집 경우의 수 모두 출력
-    myCityDistance = 0
-    for h in house_pos:  # 각 집의 치킨거리를 구함
-        myChickenDistance = float('inf')
-        for _s in s:
-            myChickenDistance = min(
-                getChickenDistance(h, _s), myChickenDistance)
-        myCityDistance += myChickenDistance
-    answer = min(answer, myCityDistance)
+for s in combinations(virus_pos, m):
+    res = bfs(n, board, s, zero_count)
+    if(res != -1):
+        answer = min(answer, res)
 
-print(answer)
+if(answer == float("inf")):  # 전부 -1이었던것..
+    print(-1)
+else:
+    print(answer)
