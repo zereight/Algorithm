70525669a25409425db1981dca7e5752f9f5b470
zereight <42544600+zereight@users.noreply.github.com>
2020-10-27 01:46:08 +0900

    9372 상근이의 여행 dfs

--- main.py
+++ main.py
@@ -1,39 +1,31 @@
 import sys
-sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
-n, b = map(int, input().rstrip().split(" "))
 
-matrix = []
-for i in range(n):
-    matrix.append(
-        [*map(lambda x: x % 1000, map(int, input().rstrip().split(" ")))])
-
-
-def productMatrix(arr1, arr2):
-    answer = []
-    for idx1 in range(len(arr1)):
-        row = []
-        for idx2 in range(len(arr2[0])):
-            tmp = 0
-            for idx3 in range(len(arr1[0])):
-                tmp += arr1[idx1][idx3] % 1000 * arr2[idx3][idx2] % 1000
-                tmp = tmp % 1000
-            row.append(tmp)
-        answer.append(row)
-    return answer
+def dfs(curr):
+    global answer
+    answer += 1
+    for dest in graph[curr]:
+        if(visited[dest] == 0):
+            visited[dest] = 1
+            dfs(dest)
 
 
-def solve(tmp_b):
-    global matrix
-    if(tmp_b == 1):
-        return matrix
-    t = solve(int(tmp_b/2))
-    t = productMatrix(t, t)
-    if(tmp_b % 2 != 0):
-        t = productMatrix(t, matrix)
-    return t
+answer, graph, visited = None, None, None
+answers = []
+T = int(input().rstrip())
+for _ in range(T):
+    answer = 0
+    n, m = map(int, input().rstrip().split(" "))
+    graph = [[] for _ in range(n+1)]
+    visited = [0]*(n+1)
+    for _ in range(m):
+        start, end = map(int, input().rstrip().split(" "))
+        graph[start].append(end)
+        graph[end].append(start)
+    visited[1] = 1
+    dfs(1)
+    answers.append(answer)
 
-
-for m in solve(b):
-    print(" ".join(map(str, m)))
+for a in answers:
+    print(a-1)
