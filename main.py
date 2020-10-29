5725df3d5e4d0cab6e02cb99ab70f17f906ecc2e
zereight <42544600+zereight@users.noreply.github.com>
2020-10-29 23:35:05 +0900

    18258 큐2

--- main.py
+++ main.py
@@ -2,15 +2,39 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
+q = deque()
+s = 0
+
 n = int(input().rstrip())
 
-cards = deque()
-for i in range(1, n+1):
-    cards.append(i)
-
-while(len(cards) != 1):
-    cards.popleft()  # 제일 위에 있는 카드를 버린다.
-    # 제일 위에 있는 카드를 제일 아래로 옮긴다.
-    cards.append(cards.popleft())
-
-print(cards[0])
+for _ in range(n):
+    order = input().rstrip().split(" ")
+    if(len(order) == 2):
+        # 무조건 push
+        q.append(order[1])
+        s += 1
+    else:
+        if(order[0] == "front"):
+            if(s == 0):
+                print(-1)
+            else:
+                print(q[0])
+        elif(order[0] == "back"):
+            if(s == 0):
+                print(-1)
+            else:
+                print(q[-1])
+        elif(order[0] == "empty"):
+            if(s):
+                print(0)
+            else:
+                print(1)
+        elif(order[0] == "size"):
+            print(s)
+        elif(order[0] == "pop"):
+            if(s == 0):
+                print(-1)
+            else:
+                s -= 1
+                print(q.popleft())
+    # print(q)
