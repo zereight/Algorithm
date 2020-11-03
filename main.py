67ede3208bd31c3bc5d042d7a7d92da4a822b2fe
zereight <42544600+zereight@users.noreply.github.com>
2020-11-03 22:46:03 +0900

    1010 다리놓기 (조합)

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import sys
+from itertools import combinations
+import operator as op
+from functools import reduce
+
 input = sys.stdin.readline
 
-n, m = map(int, input().rstrip().split(" "))
 
-board = [[*map(int, input().rstrip().split(" "))] for _ in range(n)]
-dp = [[0]*m for _ in range(n)]
+def nCr(n, r):
+    if n < 1 or r < 0 or n < r:
+        raise ValueError
+    r = min(r, n-r)
+    numerator = reduce(op.mul, range(n, n-r, -1), 1)
+    denominator = reduce(op.mul, range(1, r+1), 1)
+    return numerator // denominator
 
-dp[0][0] = board[0][0]
 
-for i in range(n):
-    for j in range(m):
-        for a, b in ((1, 0), (0, 1), (1, 1)):
-            new_i = i+a
-            new_j = j+b
-            if(new_i >= 0 and new_j >= 0 and new_i < n and new_j < m):
-                dp[new_i][new_j] = max(
-                    dp[new_i][new_j], board[new_i][new_j]+dp[i][j])
+T = int(input().rstrip())
 
-print(dp[n-1][m-1])
+for _ in range(T):
+    n, m = map(int, input().rstrip().split(" "))
+    print(nCr(m, n))
