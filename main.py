bd521f092416ab654992dda82c448a2e235fc05a
zereight <42544600+zereight@users.noreply.github.com>
2020-11-11 23:29:00 +0900

    11650 좌표정렬하기

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
-n = int(input())
-cnt = [0 for i in range(10000+1)]
-for i in range(n):
-    cnt[int(input())] += 1
-for i in range(1, 10000+1):
-    for j in range(cnt[i]):
-        print(i)
+n = int(input().rstrip())
+
+l = deque()
+
+for _ in range(n):
+    l.append([*map(int, input().rstrip().split(" "))])
+
+l = sorted(l, key=lambda x: (x[0], x[1]))
+
+for i in l:
+    print(" ".join(map(str, i)))
