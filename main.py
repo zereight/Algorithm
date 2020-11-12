e63502b84a65845040a014c733fec489f699dfdf
zereight <42544600+zereight@users.noreply.github.com>
2020-11-12 23:23:24 +0900

    2583 영역 구하기

--- main.py
+++ main.py
@@ -1,14 +1,47 @@
-import sys
 from collections import deque
-input = sys.stdin.readline
-n = int(input().rstrip())
 
-l = deque()
+m,n,k=map(int,input().split())
+a=[[0]*n for i in range(m)]
 
-for _ in range(n):
-    l.append([*map(int, input().rstrip().split(" "))])
+dx=[-1, 0, 1, 0]
+dy=[0, 1, 0, -1]
 
-l = sorted(l, key=lambda x: (x[0], x[1]))
+l=[]
 
+for i in range(k):
+    x1, y1, x2, y2=map(int,input().split())
+
+    for j in range(x1,x2):
+        for k in range(y1,y2):
+            a[k][j]=1
+            
+def bfs(s1,s2):
+    de=deque()
+    de.append([s1,s2])
+    a[s1][s2]=1
+    c=1
+
+    while de:
+        x,y=de.popleft()
+
+        for i in range(4):
+            nx= x + dx[i]
+            ny= y + dy[i]
+
+            if 0 <= nx < m and 0 <= ny < n:
+                if a[nx][ny] == 0:
+                    
+                    a[nx][ny] = 1
+                    c += 1
+                    de.append([nx,ny])
+    l.append(c)
+
+for i in range(m):
+    for j in range(n):
+        if a[i][j] == 0:
+            bfs(i,j)
+
+print(len(l))
+l.sort()
 for i in l:
-    print(" ".join(map(str, i)))
+    print(i,end=' ')
