156182b908444cb76800a2d81043f99caa0ea7ff
zereight <42544600+zereight@users.noreply.github.com>
2020-11-13 23:41:25 +0900

    1026 보물

--- main.py
+++ main.py
@@ -1,47 +1,13 @@
-from collections import deque
+import sys
+input = sys.stdin.readline
 
-m,n,k=map(int,input().split())
-a=[[0]*n for i in range(m)]
+answer = 0
 
-dx=[-1, 0, 1, 0]
-dy=[0, 1, 0, -1]
+n = int(input().rstrip())
+A = sorted([*map(int, input().rstrip().split(" "))], reverse=True)
+B = sorted([*map(int, input().rstrip().split(" "))])
 
-l=[]
+for i in range(n):
+    answer += A[i]*B[i]
 
-for i in range(k):
-    x1, y1, x2, y2=map(int,input().split())
-
-    for j in range(x1,x2):
-        for k in range(y1,y2):
-            a[k][j]=1
-            
-def bfs(s1,s2):
-    de=deque()
-    de.append([s1,s2])
-    a[s1][s2]=1
-    c=1
-
-    while de:
-        x,y=de.popleft()
-
-        for i in range(4):
-            nx= x + dx[i]
-            ny= y + dy[i]
-
-            if 0 <= nx < m and 0 <= ny < n:
-                if a[nx][ny] == 0:
-                    
-                    a[nx][ny] = 1
-                    c += 1
-                    de.append([nx,ny])
-    l.append(c)
-
-for i in range(m):
-    for j in range(n):
-        if a[i][j] == 0:
-            bfs(i,j)
-
-print(len(l))
-l.sort()
-for i in l:
-    print(i,end=' ')
+print(answer)
