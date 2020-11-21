be7fdc0b472ba93fa561c141cf6b106335602867
zereight <42544600+zereight@users.noreply.github.com>
2020-11-22 00:45:20 +0900

    [Programmers] 3진법 뒤집기

--- main.py
+++ main.py
@@ -1,27 +1,10 @@
-function getAllCombinations(arr, m) {
-    let res = []
-    for(let i=0; i<arr.length-1; i++){
-        for(let j=i+1; j<arr.length; j++){
-            res.push( [arr[i], arr[j]] );
-        }
-    }
-    return res;
-}
-function solution(numbers) {
-    var answer = [];
-    
-    getAllCombinations(numbers, 2).map(
-        a => {
-            answer.push(a[0]+a[1])
-        }
-    )
-    
-    answer = [...new Set(answer)]
-    answer.sort(
-        (_a1, _a2) => _a1-_a2
-    );
-    
+function solution(n) {
+    var answer = 0;
+    let temp_3 = n.toString(3);
+    temp_3 = temp_3.split("").reverse().join("");
+
+    answer = Number.parseInt(temp_3, 3);
+
+
     return answer;
 }
-
-console.log(solution([5,0,2,7]))
