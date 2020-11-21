9b3c884ec197d9ade7ac1f160d9addf60d47d0ec
zereight <42544600+zereight@users.noreply.github.com>
2020-11-21 23:40:56 +0900

    [Programmers] 두개 뽑아서 더하기 javascript

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-import sys
-input = sys.stdin.readline
+function getAllCombinations(arr, m) {
+    let res = []
+    for(let i=0; i<arr.length-1; i++){
+        for(let j=i+1; j<arr.length; j++){
+            res.push( [arr[i], arr[j]] );
+        }
+    }
+    return res;
+}
+function solution(numbers) {
+    var answer = [];
+    
+    getAllCombinations(numbers, 2).map(
+        a => {
+            answer.push(a[0]+a[1])
+        }
+    )
+    
+    answer = [...new Set(answer)]
+    answer.sort(
+        (_a1, _a2) => _a1-_a2
+    );
+    
+    return answer;
+}
 
-n = int(input().rstrip())
-points = []
-for _ in range(n):
-    points.append([*map(int, input().rstrip().split(" "))])
-
-points = sorted(points, key=lambda p: (p[1], p[0]))
-
-for p in points:
-    print(" ".join([*map(str, p)]))
+console.log(solution([5,0,2,7]))
