2f3d119eb84784a2bb7448d70109a14a837581b7
zereight <42544600+zereight@users.noreply.github.com>
2020-11-24 23:09:12 +0900

    7568 덩치 (js)

--- main.py
+++ main.py
@@ -6,14 +6,23 @@ const rl = readline.createInterface({
 
 const solution = (l) => {
 
-    let cnt = 1;
-    l.forEach(e => {
-        let answer = 0;
-        e.split(" ").map(ee => { answer += parseInt(ee) });
-        console.log(`Case #${cnt}: ${answer}`);
-        cnt++;
+    l = l.map(_l => {
+        let [a, b] = _l.split(" ");
+        return [parseInt(a), parseInt(b)];
     });
 
+    const answer = []
+    l.forEach(_l => {
+        let cnt = 0;
+        l.forEach(e => {
+            if (e[0] > _l[0] && e[1] > _l[1]) {
+                cnt++;
+            }
+        });
+        answer.push(cnt + 1);
+    })
+
+    console.log(answer.join(" "))
 }
 
 const input = [];
@@ -21,7 +30,6 @@ rl.on("line", function(line) {
     input.push(line);
 
 }).on("close", function() {
-
 
     solution(input.slice(1));
     process.exit();
