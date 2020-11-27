faa90e4e9f80a53a2daa31e2bcc7b49a13584c53
zereight <42544600+zereight@users.noreply.github.com>
2020-11-27 23:46:37 +0900

    11728 배열 합치기 (js)

--- main.py
+++ main.py
@@ -4,39 +4,43 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-const solution = (students) => {
-    students = students.map(student => {
-        const res = student.split(" ");
-        res[1] = parseInt(res[1]);
-        res[2] = parseInt(res[2]);
-        res[3] = parseInt(res[3]);
-        return res;
-    });
-    students.sort(
-        (a, b) => {
-            if (a[1] !== b[1]) {
-                return -(a[1] - b[1]);
-            } else if (a[2] !== b[2]) {
-                return (a[2] - b[2]);
-            } else if (a[3] !== b[3]) {
-                return -(a[3] - b[3]);
+const solution = (a, b) => {
+    const aLength = a.length,
+        bLength = b.length;
+    const answer = [];
+    let aIndex = 0,
+        bIndex = 0;
+    a = a.map(e => parseInt(e));
+    b = b.map(e => parseInt(e));
+    while (aIndex < aLength || bIndex < bLength) {
+        if (aIndex < aLength && bIndex < bLength) {
+            if (a[aIndex] > b[bIndex]) {
+                answer.push(b[bIndex]);
+                bIndex++;
             } else {
-                if (a[0] < b[0]) {
-                    return -1;
-                } else if (a[0] > b[0]) {
-                    return 1;
-                } else {
-                    return 0;
-                }
+                answer.push(a[aIndex]);
+                aIndex++;
             }
-
+        } else {
+            if (aIndex < aLength) {
+                // while (aIndex < aLength) {
+                //     answer.push(a[aIndex]);
+                //     aIndex++;
+                // }
+                answer.push(...a.slice(aIndex));
+                aIndex = aLength;
+            } else {
+                // while (bIndex < bLength) {
+                //     answer.push(b[bIndex]);
+                //     bIndex++;
+                // }
+                answer.push(...b.slice(bIndex));
+                bIndex = bLength;
+            }
         }
-    );
-    let answer = "";
-    for (let i = 0; i < students.length; i++) {
-        answer += `${students[i][0]}\n`;
+
     }
-    console.log(answer);
+    console.log(answer.join(" "));
 }
 
 const input = [];
@@ -44,6 +48,9 @@ rl.on("line", function(line) {
     input.push(line);
 
 }).on("close", function() {
-    solution(input.slice(1));
+    const a = input[1].length === 1 ? [input[1]] : input[1].split(" ");
+    const b = input[2].length === 1 ? [input[2]] : input[2].split(" ");
+    delete input;
+    solution(a, b);
     process.exit();
 });
