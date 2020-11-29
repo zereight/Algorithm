0d1be2bb21259b17ed04a0407de2bf6105dd34d2
zereight <42544600+zereight@users.noreply.github.com>
2020-11-29 18:20:35 +0900

    1927 최소 힙 (js)

--- main.py
+++ main.py
@@ -1,50 +1,76 @@
+'use strict'
+
+function MinHeap() {
+    this.heap = [0];
+
+    this.insert = (v) => {
+        this.heap.push(v);
+        let p = this.heap.length - 1;
+        while (p > 1 && this.heap[Math.floor(p / 2)] > this.heap[p]) {
+            let tmp = this.heap[Math.floor(p / 2)];
+            this.heap[Math.floor(p / 2)] = this.heap[p];
+            this.heap[p] = tmp;
+            p = Math.floor(p / 2);
+        }
+    };
+    this.getLength = () => {
+        return this.heap.length;
+    }
+    this.delete = () => {
+        if (this.heap.length - 1 < 1) {
+            return 0;
+        }
+        let deletedItem = this.heap[1];
+
+        this.heap[1] = this.heap[this.heap.length - 1];
+        this.heap.pop();
+
+        let p = 1;
+        while (p * 2 < this.heap.length) {
+            let min = this.heap[p * 2];
+            let minP = p * 2;
+            if (p * 2 + 1 < this.heap.length && min > this.heap[p * 2 + 1]) {
+                min = this.heap[p * 2 + 1];
+                minP = p * 2 + 1;
+            }
+            if (this.heap[p] < min) {
+                break;
+            }
+
+            let tmp = this.heap[p];
+            this.heap[p] = this.heap[minP];
+            this.heap[minP] = tmp;
+            p = minP;
+        }
+        return deletedItem;
+    };
+}
+
+
 const readline = require("readline");
+const {
+    parse
+} = require("path");
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
 });
 
-const solution = (string) => {
-    const info = new Array(string.length).fill(0);
-    let canFlip = true;
-    let firstFind = false;
-    let answer = '';
-    let start = 0;
-    let end = 0;
-    for (let i = 0; i < string.length; i++) {
-        if (string[i] === "<") {
-            canFlip = false;
-        } else if (string[i] === ">") {
-            canFlip = true;
+const solution = (input) => {
+    const q = new MinHeap();
+    let answer = "";
+    input.forEach(element => {
+        element = parseInt(element);
+        if (element > 0) {
+            q.insert(element);
         } else {
-            if (canFlip && string[i] !== " ") {
-                info[i] = 1;
+            if (q.getLength() === 0) {
+                answer += "0\n";
+            } else {
+                answer += `${q.delete()}\n`;
             }
         }
-    }
-    for (let i = 0; i < info.length;) {
-        if (info[i] === 1) {
-            if (!firstFind) {
-                firstFind = true;
-                start = i;
-            }
-            i++;
-        } else {
-            if (firstFind) {
-                end = i;
-                answer += Array.from(string.slice(start, end)).reverse().join("");
-                firstFind = false;
-            }
-
-            answer += string[i];
-            i++;
-        }
-    }
-    if (firstFind) {
-        end = info.lenght;
-        answer += Array.from(string.slice(start, end)).reverse().join("");
-        firstFind = false;
-    }
+    });
     console.log(answer);
 };
 
@@ -52,6 +78,6 @@ const input = [];
 rl.on("line", function (line) {
     input.push(line);
 }).on("close", function () {
-    solution(input[0]);
+    solution(input.slice(1));
     process.exit();
 });
