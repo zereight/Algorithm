c267a9bfc7062e0901d5ce74cc9d479199b43679
zereight <fullstackmachinelearning@gmail.com>
2020-12-07 23:46:26 +0900

    init

--- ps.py
+++ ps.py
@@ -1,141 +1,41 @@
-'use strict'
+import sys
+from collections import deque
+from itertools import combinations
+import copy
+input = sys.stdin.readline
 
-function PriorityQueue(comparator) { //https://github.com/janogonzalez/priorityqueuejs/blob/master/index.js
-
-    this.defaultComparator = function (a, b) {
-        if (typeof a === "number" && typeof b === "number") {
-            return a - b;
-        } else {
-            a = a.toString();
-            b = b.toString();
-            if (a == b) return 0;
-            return a > b ? 1 : -1;
-        }
-    }
-
-    const _comparator = comparator || this.defaultComparator;
-    const _elements = [];
-
-    const _compare = function (a, b) {
-        return _comparator(_elements[a], _elements[b]);
-    }
-
-    const _swap = function (a, b) {
-        const tmp = _elements[a];
-        _elements[a] = _elements[b];
-        _elements[b] = tmp;
-    }
+n, m = map(int, input().rstrip().split(" "))
+board = []
+virusPos = []
+nothingPos = []
+direction = [
+    []
+]
+for row in range(n):
+    board.append(input().rstrip().split(" "))
+    for col in range(m):
+        if(board[row][col] == 0):
+            nothingPos.append([row, col])
+        elif(board[row][col] == 1):
+            virusPos.append([row, col])
 
 
-
-    this.getSize = function () {
-        return _elements.length;
-    }
-    this.isEmpty = function () {
-        return this.getSize() === 0;
-    }
-
-    this.peek = function () {
-        try {
-            return _elements[0];
-        } catch (e) {
-            throw new Error("queue is empty");
-        }
-    }
-
-    this.dequeue = function () {
-        if (this.getSize() === 0) {
-            throw new Error("Empty queue");
-        }
-
-        let first = this.peek();
-        let last = _elements.pop(); // 1개 뺌
-        let size = this.getSize();
-
-        if (size === 0) return last; // 1개 빼서 0개이면 마지막으로 뺏던 last반환
-
-        _elements[0] = last;
-        let current = 0;
-
-        while (current < size) {
-            let largest = current;
-            let left = (2 * current) + 1;
-            let right = (2 * current) + 2;
-
-            if (left < size && _compare(left, largest) >= 0) {
-                largest = left;
-            }
-
-            if (right < size && _compare(right, largest) >= 0) {
-                largest = right;
-            }
-
-            if (largest === current) break;
-
-            _swap(largest, current);
-            current = largest;
-        }
-
-        return first;
-    }
-
-    this.enqueue = function (elem) {
-        let size = _elements.push(elem);
-        let current = size - 1;
-
-        while (current > 0) {
-            const parent = Math.floor((current - 1) / 2);
-            if (_compare(current, parent) <= 0) break;
-            _swap(parent, current);
-            current = parent;
-        }
-
-        return size;
-    }
-
-    this.forEach = function (fn) {
-        return _elements.forEach(fn);
-    }
-
-    this.getArray = function () {
-        return _elements.slice();
-    }
-}
-
-const readline = require("readline");
-const {
-    parse
-} = require("path");
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
-
-const solution = function (input) {
-    const n = parseInt(input[0]);
-    const arr = input[1].split(" ").map(elem => parseInt(elem));
-    const pq = new PriorityQueue();
-    let score = 0;
-    arr.forEach(
-        a => {
-            pq.enqueue(a);
-        }
-    );
-    while (pq.getSize() > 1) {
-        const max1 = pq.dequeue();
-        const max2 = pq.dequeue();
-
-        pq.enqueue(max1 + max2);
-        score += max1 * max2;
-    }
-    console.log(score);
-};
+def bfs(wallPos, board, virusPos):
+    newBoard = copy.deepcopy(board)
+    for x, y in wallPos:
+        newBoard[x][y] = 1
+    zeroCnt = n*m - len(wallPos) - len(virusPos)
+    q = deque(virusPos)
+    while(q):
+        curr_x, curr_y = q.popleft()
+        for a, b in direction:
+            nx, ny = curr_x+a, curr_y+b
+            if(nx >= 0 and ny >= 0 and nx < n and ny < m):
+                if(board[nx][ny] == 0):
+                    q.append([nx, ny])
+                    board[nx][ny] = 2
+                    zeroCnt -= 1
 
 
-const input = [];
-rl.on("line", function (line) {
-    input.push(line);
-}).on("close", function () {
-    solution(input);
-    process.exit();
-});
+for wallPos in combinations(nothingPos, 3):
+    pass
