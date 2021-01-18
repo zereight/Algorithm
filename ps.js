dec04f38eee1ef197f7300072191fd9f7f598348
zereight <42544600+zereight@users.noreply.github.com>
2021-01-19 01:04:07 +0900

    [LeetCode] 13. Roman to Integer

--- ps.js
+++ ps.js
@@ -1,11 +1,51 @@
 /**
- * @param {number} x
+ * @param {string} s
  * @return {number}
  */
-var reverse = function (x) {
-	const limits = [-1 * 2 ** 31, 2 ** 31 - 1];
-	let ans = parseInt(String(x).split('').reverse().join(''));
-	ans = x > 0 ? ans : -ans;
-	if (limits[0] > ans || limits[1] < ans) return 0;
-	return ans;
+
+const getValue = (previousChr, currChr) => {
+    switch(currChr){
+        case 'I':
+            return 1;
+        case 'V':
+            if(previousChr==='I'){
+                return 4-1;
+            }
+            return 5;
+        case 'X':
+            if(previousChr==='I'){
+                return 9-1;
+            }
+            return 10;
+        case 'L':
+            if(previousChr==='X'){
+                return 40-10;
+            }
+            return 50;
+        case 'C':
+            if(previousChr==='X'){
+                return 90-10;
+            }
+            return 100;
+        case 'D':
+            if(previousChr==='C'){
+                return 400-100;
+            }
+            return 500;
+        case 'M':
+            if(previousChr==='C'){
+                return 900-100;
+            }
+            return 1000;
+    }
+}
+
+var romanToInt = function(s) {
+    let ans = 0;
+    let prev = '';
+    for(const chr of s){
+        ans += getValue(prev, chr);
+        prev = chr;
+    }
+    return ans;
 };
