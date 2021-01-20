0133e8ae3c5d6988dab831025e01ea3a27f44bc9
zereight <42544600+zereight@users.noreply.github.com>
2021-01-20 11:54:41 +0900

    [LeetCode] 20. Valid Parentheses

--- ps.js
+++ ps.js
@@ -1,31 +1,20 @@
 /**
- * @param {string[]} strs
- * @return {string}
+ * @param {string} s
+ * @return {boolean}
  */
-
-const findMinLength = (array) =>
-	array.reduce((a, b) => (a.length < b.length ? a : b));
-
-const isSameChrOfIndex = (index, strs) => {
-	const pivot = strs[0][index];
-	for (const str of strs.slice(1)) {
-		if (str[index] !== pivot) return false;
-	}
-	return true;
-};
-
-var longestCommonPrefix = function (strs) {
-    if(strs.length === 0) return '';
-	const minLength = findMinLength(strs).length;
-	let ans = '';
-	let temp = '';
-	for (let index = 0; index < minLength; index++) {
-		if (!isSameChrOfIndex(index, strs)) {
-            break;
-			
+var isValid = function (s) {
+	const stack = [];
+	for (const chr of Array.from(s)) {
+		if (chr === '(' || chr === '[' || chr === '{') {
+			stack.push(chr);
+		} else if (chr === ')') {
+			if (stack.pop() !== '(') return false;
+		} else if (chr === ']') {
+			if (stack.pop() !== '[') return false;
+		} else if (chr === '}') {
+			if (stack.pop() !== '{') return false;
 		}
-        temp += strs[0][index];
-        ans = ans.length < temp.length ? temp : ans;
 	}
-	return ans;
+	if (stack.length > 0) return false;
+	return true;
 };
