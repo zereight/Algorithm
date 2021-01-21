e8d9931396fcf92bb99dcd8cbc85d419b2ac7ed0
zereight <42544600+zereight@users.noreply.github.com>
2021-01-22 00:11:22 +0900

    [LeetCode] 8. String to Integer (atoi)

--- ps.js
+++ ps.js
@@ -1,20 +1,33 @@
 /**
  * @param {string} s
- * @return {boolean}
+ * @return {number}
  */
-var isValid = function (s) {
-	const stack = [];
-	for (const chr of Array.from(s)) {
-		if (chr === '(' || chr === '[' || chr === '{') {
-			stack.push(chr);
-		} else if (chr === ')') {
-			if (stack.pop() !== '(') return false;
-		} else if (chr === ']') {
-			if (stack.pop() !== '[') return false;
-		} else if (chr === '}') {
-			if (stack.pop() !== '{') return false;
+var myAtoi = function (s) {
+	let isNegative = false;
+	let digitRange = 0;
+	s = s.trim(' ');
+	if (Array.from(s)[0] === '-') isNegative = true;
+	Array.from(s).some((v, i) => {
+		
+		if(v === '-' || v === '+'){
+			if(i!==0){
+				return true;
+			}
 		}
-	}
-	if (stack.length > 0) return false;
-	return true;
+		
+		if ('0' <= v || v <= '9') {
+			digitRange = i;
+		} else {
+			return true;
+		}
+		return false;
+	});
+
+	s = s.slice(0, digitRange + 1).trim('0');
+	s = parseInt(s, 10);
+	s = Math.max(-1 * 2 ** 31, s);
+	s = Math.min(2 ** 31 - 1, s);
+	return s ? s : 0;
 };
+
+console.log(myAtoi('words and 987'));
