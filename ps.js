4f22cbcfe5e6e9ec92b375a79e6063b472574a11
zereight <42544600+zereight@users.noreply.github.com>
2021-01-27 16:41:41 +0900

    101. Symmetric Tree

--- ps.js
+++ ps.js
@@ -1,14 +1,27 @@
 /**
- * @param {number[]} prices
- * @return {number}
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
  */
-var maxProfit = function(prices) {
-    let maxProfit = 0;
-    for(let i=1; i<prices.length; i++){
-        if(prices[i] > prices[i-1]){
-            maxProfit += prices[i] - prices[i-1];
-        }
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+    const q = [root, root];
+    while(q.length > 0){
+        const [node1, node2] = [q.shift(), q.shift()];
+        if(node1 === null && node2 === null) continue;
+        if(node1 === null || node2 === null) return false;
+        if(node1.val !== node2.val) return false;
         
+        q.push(node1.left);
+        q.push(node2.right);
+        q.push(node1.right);
+        q.push(node2.left);
     }
-    return maxProfit;
+    return true;
 };
