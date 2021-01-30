62c0e00b85e2616124b14ec6c833ba53ea1d72ac
zereight <42544600+zereight@users.noreply.github.com>
2021-01-30 23:16:28 +0900

    414. Third Maximum Number

--- ps.js
+++ ps.js
@@ -1,27 +1,12 @@
 /**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
+ * @param {number[]} nums
+ * @return {number}
  */
-/**
- * @param {TreeNode} root
- * @return {boolean}
- */
-var isSymmetric = function(root) {
-    const q = [root, root];
-    while(q.length > 0){
-        const [node1, node2] = [q.shift(), q.shift()];
-        if(node1 === null && node2 === null) continue;
-        if(node1 === null || node2 === null) return false;
-        if(node1.val !== node2.val) return false;
-        
-        q.push(node1.left);
-        q.push(node2.right);
-        q.push(node1.right);
-        q.push(node2.left);
+var thirdMax = function(nums) {
+    nums = Array.from(new Set(nums));
+    nums.sort((a,b)=>a-b);
+    if(nums.length < 3){
+        return nums[nums.length - 1];
     }
-    return true;
+    return nums[nums.length - 3];
 };
