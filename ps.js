0493a372f16039f2a78f821bc73932d430cb1c56
zereight <42544600+zereight@users.noreply.github.com>
2021-02-02 23:03:47 +0900

    859. Buddy Strings

--- ps.js
+++ ps.js
@@ -1,12 +1,24 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
-var thirdMax = function(nums) {
-    nums = Array.from(new Set(nums));
-    nums.sort((a,b)=>a-b);
-    if(nums.length < 3){
-        return nums[nums.length - 1];
+var buddyStrings = function(A, B) {
+    if (A.length != B.length) {
+        return false;
     }
-    return nums[nums.length - 3];
+    
+    if (A == B) {
+        let s = new Set();
+        
+        for (let i = 0; i < A.length; i++) {
+            s.add(A.charAt(i));
+        }
+        
+        return s.size < A.length;
+    }
+    
+    let diff = [];
+    for (let i = 0; i < A.length; i++) {
+        if (A.charAt(i) != B.charAt(i)) {
+            diff.push(i);
+        }
+    }
+    
+    return diff.length == 2 && A.charAt(diff[0]) == B.charAt(diff[1]) && A.charAt(diff[1]) == B.charAt(diff[0]);
 };
