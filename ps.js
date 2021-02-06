68030182f72d397d08869033802011b6db9c972b
zereight <42544600+zereight@users.noreply.github.com>
2021-02-06 23:01:17 +0900

    1464. Maximum Product of Two Elements in an Array

--- ps.js
+++ ps.js
@@ -1,52 +1,15 @@
 /**
- * @param {character[][]} board
- * @return {boolean}
+ * @param {number[]} nums
+ * @return {number}
  */
-
-const checkRow = (board) => {
-    for(let row=0; row<board.length; row++){
-        const visited = Array.from({length: 0}, ()=>false);
-        for(let col=0; col<board.length; col++){
-            if(!('0'<=board[row][col] && board[row][col]<='9')) continue;
-            if(visited[parseInt(board[row][col])]) return false;
-            visited[parseInt(board[row][col])] = true;
-        }
-    }
-    return true;
-}
-
-const checkCol = (board) => {
-    for(let col=0; col<board.length; col++){
-        const visited = Array.from({length: 0}, ()=>false);
-        for(let row=0; row<board.length; row++){
-            if(!('0'<=board[row][col] && board[row][col]<='9')) continue;
-            if(visited[parseInt(board[row][col])]) return false;
-            visited[parseInt(board[row][col])] = true;
-        }
-    }   
-    return true;
-}
-
-const checkBox = (board) => {
-    const points = [[0,0],[0,3],[0,6],
-                   [3,0],[3,3],[3,6],
-                   [6,0],[6,3],[6,6],];
-    for(let i=0; i<9; i++){
-        const [a,b] = points[i];
-        const visited = Array.from({length: 0}, ()=>false);
-        for(let j=0; j<3; j++){
-            for(let k=0; k<3; k++){
-                const [row, col] = [j+a, k+b];
-                if(!('0'<=board[row][col] && board[row][col]<='9')) continue;
-                if(visited[parseInt(board[row][col])]) return false;
-                visited[parseInt(board[row][col])] = true;
+var maxProduct = function(nums) {
+    let maxNumber = 0;
+    nums.forEach((a,i) => {
+        nums.forEach((b,j) => {
+            if(i!==j){
+                maxNumber = Math.max(maxNumber, (a-1)*(b-1));    
             }
-        }
-    }
-    return true;
-}
-
-var isValidSudoku = function(board) {
-    console.log(checkRow(board), checkCol(board), checkBox(board))
-    return checkRow(board) && checkCol(board) && checkBox(board);
+        })
+    })
+    return maxNumber;
 };
