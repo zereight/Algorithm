cbf37260203d5ffbaa72dc936b5312a7e585f9f2
zereight <fullstackmachinelearning@gmail.com>
2021-03-22 00:11:56 +0900

    1965 상자넣

--- ps.js
+++ ps.js
@@ -1,15 +1,31 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
-var maxProduct = function(nums) {
-    let maxNumber = 0;
-    nums.forEach((a,i) => {
-        nums.forEach((b,j) => {
-            if(i!==j){
-                maxNumber = Math.max(maxNumber, (a-1)*(b-1));    
-            }
-        })
-    })
-    return maxNumber;
+const readline = require('readline');
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
+
+const solution = function (input) {
+	const n = Number(input.shift());
+	const boxSize = input.shift().split(' ').map(Number);
+	const dp = Array.from({ length: n }, () => 1);
+
+	boxSize.forEach((box, index) => {
+		if (index === 0) return;
+
+		for (let i = 0; i < index; i++) {
+			if (boxSize[i] < boxSize[index] && dp[index] < dp[i] + 1) {
+				dp[index] = dp[i] + 1;
+			}
+		}
+	});
+
+	console.log(dp.reduce((a, b) => Math.max(a, b)));
 };
+
+const input = [];
+rl.on('line', function (line) {
+	input.push(line);
+}).on('close', function () {
+	solution(input);
+	process.exit();
+});
---
+++ template/배열최대최소.js
@@ -0,0 +1,2 @@
+const findMax = (array) => array.reduce((a, b) => Math.max(a, b));
+const findMin = (array) => array.reduce((a, b) => Math.min(a, b));
