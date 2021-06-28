89fed203f902bd1a1f493c5cd138677a139ed038
zereight <fullstackmachinelearning@gmail.com>
2021-06-28 18:07:36 +0900

    프로그래머스 - 게임 맵 최단거리

--- ps.js
+++ ps.js
@@ -1,17 +1,63 @@
+const bfs = (maps) => {
 
-const solution = (s) => {
-	const stack = [];
-	for(const chr of s){
-		if(stack[stack.length - 1] === chr){
-			stack.pop();
-		}else{
-			stack.push(chr);
-		}
+	const direction = [
+		[-1,0],
+		[1,0],
+		[0,-1],
+		[0,1]
+	];
+
+	const isValidPoint = (x,y) => {
+		const row = maps.length;
+		const col = maps[0].length;
+
+		return 0<=x && x<row && 0<=y && y<col;
 	}
-	return stack.length > 0 ? 0 : 1;
+
+	const visited = Array.from({length: maps.length}, (_, idx) => {
+		return Array.from({length: maps[idx].length}, () => 0)
+	});
+	const dp = Array.from({length: maps.length}, (_, idx) => {
+		return Array.from({length: maps[idx].length}, () => 0)
+	});
+
+	const startPoint = [0,0];
+	const destPoint = [maps.length - 1, maps[0].length - 1]
+
+	const q = [startPoint];
+
+	visited[startPoint[0]][startPoint[1]] = 1;
+	dp[startPoint[0]][startPoint[1]] = 1;
+
+	while(q.length > 0){
+		const [currX, currY] = q.shift();
+		
+		// if(destPoint[0] === currX && destPoint[1] === currY){
+		// 	break;
+		// }
+
+		for(const [deltaX, deltaY] of direction) {
+			const [nx, ny] = [currX + deltaX, currY + deltaY];
+
+			if(!isValidPoint(nx,ny)) continue;
+			if(maps[nx][ny] === 0) continue;
+			if(visited[nx][ny]) continue;
+
+			visited[nx][ny] = 1;
+			dp[nx][ny] = dp[currX][currY] + 1;
+			q.push([nx, ny]);
+		};
+
+		
+	}
+
+	return dp[destPoint[0]][destPoint[1]] > 0 ? dp[destPoint[0]][destPoint[1]] : -1;
+}
+
+const solution = (maps) => {
+    return bfs(maps);
 }
 
 
-console.log(solution("baabaa"));
-console.log(solution("cdcd"));
-console.log(solution("abcdeabcde".repeat(100000)));
+console.log(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]));
+console.log(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]));
---
+++ template/중복된2개제거.js
@@ -0,0 +1,17 @@
+
+const solution = (s) => {
+	const stack = [];
+	for(const chr of s){
+		if(stack[stack.length - 1] === chr){
+			stack.pop();
+		}else{
+			stack.push(chr)
+		}
+	}
+	return stack.length > 0 ? 0 : 1;
+}
+
+
+console.log(solution("baabaa"));
+console.log(solution("cdcd"));
+console.log(solution("abcdeabcde".repeat(100000)));
