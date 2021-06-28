fe201e85a635ddec2502f8231d11e61803dd7680
zereight <fullstackmachinelearning@gmail.com>
2021-06-28 17:21:04 +0900

    프로그래머스 - 짝지어 제거하기

--- ps.js
+++ ps.js
@@ -1,24 +1,17 @@
-const sum = (arr) => arr.reduce((acc, curr)=> acc+curr, 0)
 
-const solution = (numbers, target) => {
-	let answer = 0;
-
-	const dfs = (depth, arr) => {
-		if(depth === numbers.length){
-			if(sum(arr) === target){
-				answer++;
-			}
-			return;
+const solution = (s) => {
+	const stack = [];
+	for(const chr of s){
+		if(stack[stack.length - 1] === chr){
+			stack.pop();
+		}else{
+			stack.push(chr);
 		}
-
-		dfs(depth+1, [...arr, numbers[depth]]);
-		dfs(depth+1, [...arr, -numbers[depth]]);
 	}
-
-	dfs(0, []);
-
-	return answer;
+	return stack.length > 0 ? 0 : 1;
 }
 
-console.log(solution([1,1,1,1,1],3));
-console.log(solution([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],3));
+
+console.log(solution("baabaa"));
+console.log(solution("cdcd"));
+console.log(solution("abcdeabcde".repeat(100000)));
