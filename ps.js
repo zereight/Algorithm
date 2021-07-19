b975e0f15b05098e8856050c57b1fa9f1ce87527
zereight <fullstackmachinelearning@gmail.com>
2021-07-19 20:25:46 +0900

    프로그래머스 - 오픈채팅방

--- ps.js
+++ ps.js
@@ -1,72 +1,38 @@
-const permutation = (arr, selectNum) => {
-  let result = [];
-  if (selectNum === 1) return arr.map((v) => [v]);
+const inMessage = (name) => `${name}님이 들어왔습니다.`;
+const outMessage = (name) => `${name}님이 나갔습니다.`;
 
-  arr.forEach((v, idx, arr) => {
-    const fixer = v;
-    const restArr = arr.filter((_, index) => index !== idx);
-    const permuationArr = permutation(restArr, selectNum - 1);
-    const combineFixer = permuationArr.map((v) => [fixer, ...v]);
-    result.push(...combineFixer);
-  });
-  return result;
-};
+const logToString = (db, log) => {
+  const logString = [];
+  for (const logData of log) {
+    const [act, uid, _] = logData.split(" ");
 
-const parseExpression = (expression) => {
-  const expressArr = [];
-
-  let tmp = "";
-  Array.from(expression).forEach((chr) => {
-    if (["*", "+", "-"].some((operator) => operator === chr)) {
-      expressArr.push(tmp);
-      expressArr.push(chr);
-      tmp = "";
-    } else {
-      tmp += chr;
+    if (act === "Enter") {
+      logString.push(inMessage(db[uid]));
+    } else if (act === "Leave") {
+      logString.push(outMessage(db[uid]));
     }
-  });
-
-  expressArr.push(tmp);
-
-  return expressArr;
-};
-
-const sum = (arr) => arr.reduce((acc, curr) => acc + curr, 0);
-
-const solution = (expression) => {
-  let answer = 0;
-  const operatorCombs = permutation(["*", "+", "-"], 3);
-
-  for (const operatorComb of operatorCombs) {
-    const expressArr = parseExpression(expression);
-
-    for (const operator of operatorComb) {
-      for (let i = 0; i < expressArr.length; i++) {
-        if (expressArr[i] === operator) {
-          if (operator === "*") {
-            const newValue =
-              Number(expressArr[i - 1]) * Number(expressArr[i + 1]);
-            expressArr.splice(i - 1, 3, newValue);
-            i -= 1;
-          } else if (operator === "-") {
-            const newValue =
-              Number(expressArr[i - 1]) - Number(expressArr[i + 1]);
-            expressArr.splice(i - 1, 3, newValue);
-            i -= 1;
-          } else if (operator === "+") {
-            const newValue =
-              Number(expressArr[i - 1]) + Number(expressArr[i + 1]);
-            expressArr.splice(i - 1, 3, newValue);
-            i -= 1;
-          }
-        }
-      }
-    }
-
-    answer = Math.max(answer, Math.abs(sum(expressArr)));
   }
 
-  return answer;
+  return logString;
 };
 
-console.log(solution("100-200*300-500+20"));
+const solution = (record) => {
+  const db = {};
+
+  for (const data of record) {
+    const [act, uid, name] = data.split(" ");
+    if (act !== "Leave") db[uid] = name;
+  }
+
+  return logToString(db, record);
+};
+
+const dummy = [
+  "Enter uid1234 Muzi",
+  "Enter uid4567 Prodo",
+  "Leave uid1234",
+  "Enter uid1234 Prodo",
+  "Change uid4567 Ryan",
+];
+
+console.log(solution(dummy));
