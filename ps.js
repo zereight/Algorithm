92e892a75c67557aa7bfa0e3d26643fe9528b8c2
zereight <fullstackmachinelearning@gmail.com>
2021-08-14 21:49:54 +0900

    프로그래머스 - N진수게임

--- ps.js
+++ ps.js
@@ -1,37 +1,38 @@
-const Dec2BinString = (num) => {
-  return Number(num).toString(2);
-};
+const solution = (진법, 미리구할숫자개수, 참가인원, 내순서) => {
+  const getBaseNumber = (num) => {
+    return Number(num).toString(진법);
+  };
 
-const arrPreprocessing = (arr) => {
-  const newArr = arr.map((num) => {
-    let binString = Array.from(Dec2BinString(num));
-    if (binString.length < arr.length) {
-      const padding = Array.from(" ".repeat(arr.length - binString.length));
-      binString = [...padding, ...binString];
-    }
-    return binString.map((binChr) => (binChr === "1" ? "#" : " "));
-  });
+  let 현재십진수 = 0;
+  let 현재차례 = 0;
+  const answer = [];
+  let 남은구해야할개수 = 미리구할숫자개수 - answer.length;
 
-  return newArr;
-};
+  while (남은구해야할개수 > 0) {
+    const 십진수를변환한이진수 = `${getBaseNumber(현재십진수)}`;
 
-const solution = (n, arr1, arr2) => {
-  const newArr1 = arrPreprocessing(arr1);
-  const newArr2 = arrPreprocessing(arr2);
-  const newBoard = [];
+    for (let i = 0; i < 십진수를변환한이진수.length; i++) {
+      현재차례 = 현재차례 % 참가인원;
 
-  for (let i = 0; i < n; i++) {
-    newBoard[i] = "";
-    for (let j = 0; j < n; j++) {
-      if (newArr1[i][j] === "#" || newArr2[i][j] === "#") {
-        newBoard[i] += "#";
-      } else {
-        newBoard[i] += " ";
+      if (현재차례 === 내순서 - 1) {
+        answer.push(십진수를변환한이진수[i].toUpperCase());
+      }
+
+      현재차례++;
+
+      남은구해야할개수 = 미리구할숫자개수 - answer.length;
+
+      if (남은구해야할개수 === 0) {
+        break;
       }
     }
+
+    현재십진수++;
   }
 
-  return newBoard;
+  return answer.join("");
 };
 
-console.log(solution(5, [9, 20, 28, 18, 11], [30, 1, 21, 17, 28]));
+// console.log(solution(2, 4, 2, 1));
+// console.log(solution(16, 16, 2, 1));
+// console.log(solution(16, 16, 2, 2));
